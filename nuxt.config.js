671d0197b8fbd97317de1eca37a0993074bf05b6
AlfinRy <alfin.reynaldi26@gmail.com>
2024-08-03 19:39:54 +0700

    change portfolio link

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,31 +1,33 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Alfin Reynaldi',
+    title: "Alfin Reynaldi",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon_io/favicon.ico' },
-      { rel: "stylesheet", href: "https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"}
-    ]
+      { rel: "icon", type: "image/x-icon", href: "/favicon_io/favicon2.ico" },
+      {
+        rel: "stylesheet",
+        href: "https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css",
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/vue-type.js',
-    {src: '~/plugins/vue-confetti.js', mode: 'client'},
-    '~/plugins/vue-gtag.js',
+    "~/plugins/vue-type.js",
+    { src: "~/plugins/vue-confetti.js", mode: "client" },
+    "~/plugins/vue-gtag.js",
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -34,32 +36,33 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/ngrok',
-    ['@nuxtjs/google-analytics', {
-      id: 'G-2QBTMFDD8X'
-    }]
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/ngrok",
+    [
+      "@nuxtjs/google-analytics",
+      {
+        id: "G-2QBTMFDD8X",
+      },
+    ],
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  },
+  build: {},
   ngrok: {
     // module options
-    authtoken: '29eZrEES82BHu4MpcTtclzZYowf_38zVKwB2UGg4saXKyr16X'
+    authtoken: "29eZrEES82BHu4MpcTtclzZYowf_38zVKwB2UGg4saXKyr16X",
   },
   googleAnalytics: {
-    id: 'G-2QBTMFDD8X',
+    id: "G-2QBTMFDD8X",
     autoTracking: {
-      screenview: true
+      screenview: true,
     },
     debug: {
       enabled: true,
-      sendHitTask: true
-    } // Used as fallback if no runtime config is provided
+      sendHitTask: true,
+    }, // Used as fallback if no runtime config is provided
   },
-}
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="w-screen h-screen bg-gray-200">
     <div class="px-8 xl:px-56 h-full flex flex-col justify-center gap-5">
-      <vue-typed-js class="font-bold text-3xl xl:text-5xl flex justify-center mb-10" :startDelay="0"
- :strings="['Hello, i am Alfin :D']" :typeSpeed="100">
+      <vue-typed-js class="font-bold text-3xl xl:text-5xl flex justify-center mb-10" :startDelay="0" :strings="['Hello, i am Alfin :D']" :typeSpeed="100">
         <h1 class="typing"></h1>
       </vue-typed-js>
       <div class="w-full grid grid-cols-1 xl:grid-cols-2 text-center gap-2">
-        <a target="_blank" class="flex items-center justify-center px-5 py-2 border-2 border-black rounded" href="https://portfolio-tw.vercel.app/"><i class="text-3xl las la-address-card"></i> Portfolio</a>
+        <a target="_blank" class="flex items-center justify-center px-5 py-2 border-2 border-black rounded" href="https://portfolio-alfinry.vercel.app/"><i class="text-3xl las la-address-card"></i> Portfolio</a>
         <a target="_blank" class="flex items-center justify-center px-5 py-2 border-2 border-black rounded" href="https://www.linkedin.com/in/alfinry/"><i class="text-3xl lab la-linkedin"></i> Linkedin</a>
         <a target="_blank" class="flex items-center justify-center px-5 py-2 border-2 border-black rounded" href="https://github.com/AlfinRy"><i class="text-3xl lab la-github"></i> Github</a>
         <a target="_blank" class="flex items-center justify-center px-5 py-2 border-2 border-black rounded" href="https://instagram.com/alfinry_"><i class="text-3xl lab la-instagram"></i> Instagram</a>
@@ -22,19 +21,19 @@
 export default {
   name: 'IndexPage',
   methods: {
-    start(){
+    start() {
       this.$confetti.start({
-  particles: [
-    {
-      type: 'heart',
-    }
-  ],
-  defaultColors: [
-    'red',
-    'pink',
-    '#ba0000',
-  ],
-});
+        particles: [
+          {
+            type: 'heart',
+          }
+        ],
+        defaultColors: [
+          'red',
+          'pink',
+          '#ba0000',
+        ],
+      });
       setTimeout(() => {
         this.$confetti.stop();
       }, 3000)
